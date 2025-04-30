ea027646f309849b2d27dd27d0cef78e5a4e91a8
gabrielvictalino <gabrielvictalino@gmail.com>
2025-04-30 19:51:53 -0300

    e la vamos nos

--- app_marketoon/models.py
+++ app_marketoon/models.py
@@ -1,14 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
+
 class func_registrar_produto(models.Model):
-    nome=models.CharField(max_length=100)
-    email=models.CharField(max_length=100)
-    telefone=models.CharField(max_length=11)
-    preco=models.DecimalField(max_digits=10,decimal_places=2)
-    descricao= models.TextField(blank=True,null=True)
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)  
+    telefone = models.CharField(max_length=15)  
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    descricao = models.TextField(blank=True, null=True)
     imagem = models.ImageField(upload_to='produtos/')
-    data_criacao=models.DateTimeField(auto_now=True,verbose_name="Data de criação")
+    data_criacao = models.DateTimeField(auto_now_add=True, verbose_name="Data de criação")
+
+    def __str__(self):
+        return self.nome
 
 
 class Wishlist(models.Model):
--- marketoon/settings.py
+++ marketoon/settings.py
@@ -114,6 +114,6 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+APPEND_SLASH = True
+
